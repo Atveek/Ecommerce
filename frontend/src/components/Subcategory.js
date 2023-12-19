f3bfef45adf547b5b4e56790e7b104216c5b6327
Atveek <atveekdungarani2021@gmail.com>
2023-12-19 23:11:25 +0530

    change subcategory ui

--- frontend/src/components/Subcategory.js
+++ frontend/src/components/Subcategory.js
@@ -31,17 +31,22 @@ export default function Subcategory() {
     navigate(`/${category}/${Subcategory}`);
   };
   return (
-    <div className=" flex-grow flex flex-col justify-center items-cente">
-      <ul className="grid grid-flow-row grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 gap-5">
+    <div className="flex-grow flex flex-col justify-center items-cente my-4 mx-3 rounded-md bg-white">
+      <ul className="grid grid-flow-row justify-center grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 mx-[auto]">
         {Array.isArray(subcategorys) &&
           subcategorys.map((Subcategory) => {
             return (
               <li
-                className="p-5 shadow1 rounded-xl hover:bg-gray-300 hover:bottom-1 duration-500 cursor-pointer"
+                className="flex flex-col p-5 shadow1 rounded-xl justify-center items-center hover:bg-gray-300 text-center hover:bottom-1 duration-500 cursor-pointer"
                 key={Subcategory}
                 onClick={() => handleClick(Subcategory)}
               >
-                {Subcategory}
+                <img
+                  className="w-28 aspect-square cover-full"
+                  src="image/Electronics.png"
+                  alt="a"
+                />
+                <h2>{Subcategory}</h2>
               </li>
             );
           })}
